7a3dfe30cccda6f690bb9b52d24bb2459abd5465
J05H14 <j05h14@yahoo.com>
2017-07-04 00:07:47 -0700

    get file in gui class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,9 @@
 package application;
 	
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -11,8 +15,11 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,900,700);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -23,4 +30,17 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	public File[] getFiles(){
+		JFileChooser fc = new JFileChooser();
+		fc.setMultiSelectionEnabled(true);
+		File[] files = null;
+		int retVal = fc.showOpenDialog(null);
+		if(retVal == JFileChooser.APPROVE_OPTION){
+			files = fc.getSelectedFiles();
+		}
+		
+		return files;
+	}
+	
 }
